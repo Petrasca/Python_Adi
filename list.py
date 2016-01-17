02cfba685da1880809a5d0545e6b301e84908d8d
Petrasca <adipetrasca@yahoo.com>
2016-01-17 23:05:21 +0200

    Revert "Revert "Files from bucky's tutorials""
    This reverts commit 9b02c3e613899e573bdf4ebaf24fba7752c37e6c.

---
+++ list.py
@@ -0,0 +1,22 @@
+nr=[7,5,8,40,23,9]
+print len(nr)
+print "len returneaza lungimea sirului."
+print max(nr)
+print min(nr)
+
+print list('text to list')
+
+print "________________"
+
+print nr
+nr[3]=77
+print nr
+
+print "________________"
+
+print nr
+del nr[3]
+print nr
+print len(nr)
+
+raw_input("Press <enter> to exit")
