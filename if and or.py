3295399ecd77d90406e283c03073738d26125b09
Petrasca <adipetrasca@yahoo.com>
2016-01-17 22:51:31 +0200

    Files from bucky's tutorials
    Files were created while watching the youtube tutorials.

---
+++ if and or.py
@@ -0,0 +1,63 @@
+cuvant="text"
+if cuvant=="texxt":
+    print 'Testul e trecut'
+elif cuvant=="text":
+    print 'Testul a trecut'
+else:
+    print 'Testul nu a trecut'
+
+print "_a_____________"
+
+
+ceva="animal"
+animal="pisica"
+
+if ceva=="animal":
+    if animal=="pisica":
+        print 'animalul e pisica'
+    else:
+        print 'Nu e animal.'
+else:
+    print 'nu stiu ce animal este'
+
+
+print "_b_____________"
+
+
+a=[1,2,3]
+b=[1,2,3]
+
+print a is b
+
+a=b=[1,2,3]
+
+print a is b
+
+print "_c_____________"
+
+example = 5
+if example > 3 and example < 10:
+    print 'Nr is between 3 and 10'
+
+if example > 3 or example < 4:
+    print 'Numarul e > 3 sau < 4'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
